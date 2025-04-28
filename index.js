045a48f166f79c99816d7dc026339340068d4d0c
evaslucifer <himanish772005@gmail.com>
2025-04-28 23:31:38 +0530

    2

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log("workinbg")
+// // var state={
+// //     tasklist:[
+// //         {
+// //             imageURL:"",
+// //             taskTitle:"",
+// //             taskType:"",
+// //             taskDesc:"",
+// //         },
+// //         {
+// //             imageURL:"",
+// //             taskTitle:"",
+// //             taskType:"",
+// //             taskDesc:"",
+// //         },
+// //         {
+// //             imageURL:"",
+// //             taskTitle:"",
+// //             taskType:"",
+// //             taskDesc:"",
+// //         },
+// //         {
+// //             imageURL:"",
+// //             taskTitle:"",
+// //             taskType:"",
+// //             taskDesc:"",
+// //         },
+// //         {
+// //             imageURL:"",
+// //             taskTitle:"",
+// //             taskType:"",
+// //             taskDesc:"",
+// //         }
+// // ]
+// // };
+const state={
+    tasklist:[],
+};
+// dom operations
+const taskContents=document.querySelector(".task_contents");
+const taskModal=document.querySelector(".task__modal__body");
+// console.log(taskContents);
+// console.log(taskModal);
+const htmltaskContent=({id,title,description,type,url})=>{};
