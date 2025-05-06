abd0d40d8397146ae7ef582645b7647a8fd69ccf
evaslucifer <himanish772005@gmail.com>
2025-05-06 20:42:29 +0530

    screen card is shown

--- index.js
+++ index.js
@@ -43,8 +43,8 @@ const taskModal=document.querySelector(".task__modal__body");
 const htmlTaskContent = ({id,title,description,type,url})=> `
 <div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
   <div class='card shadow-sm task__cards'>
-    <div class='card-header dflex justify-content-end task__cards__header>
-      <button type="button " class='btn btn-outline-primary mr-1.5' name=${id}>
+    <div class='card-header  d-grid gap-2 d-md-flex justify-content-md-end task__cards__header'>
+      <button type="button" class='btn btn-outline-primary mr-1.5' name=${id}>
         <i class='fas fa-pencil-alt name=${id}'></i>   
       </button>
       <button type="button " class='btn btn-outline-danger mr-1.5' name=${id}>
@@ -56,7 +56,7 @@ const htmlTaskContent = ({id,title,description,type,url})=> `
        url &&
       `<img width=100% src=${url} alt='card image' class='card-img-top md-3 rounded-lg'/>`
      }
-      <h4 class='card-title task__card__title'>${title}</h4>
+     <h4 class='card-title task__card__title'>${title}</h4>
 
       <p class='description trim-3-lines text-muted'>${description}</p>
       <div class='tags text-white d-flex flex-wrap'>
@@ -65,7 +65,7 @@ const htmlTaskContent = ({id,title,description,type,url})=> `
     </div>
 
     <div class='card-footer'>
-      <button type='button' class='btn btn-outline-primary float-right data-bs-toggle="modal" data-bs-target="#showTask'>open task</button>
+      <button type='button' class='btn btn-outline-primary float-right' data-bs-toggle='modal' data-bs-target='#showTask'>open task</button>
     </div>
 
 
@@ -73,12 +73,12 @@ const htmlTaskContent = ({id,title,description,type,url})=> `
    </div>
 
 </div>
-`;
+;`
 
 // modal body on open task
 const htmlModalContent = ({id,title,description,url})=> {
   const date=new Date(parseInt(id));
-  return`
+  return `
    <div id=${id}>
    ${
     url &&
@@ -95,7 +95,7 @@ const htmlModalContent = ({id,title,description,url})=> {
 
 const updateLocalStorage=()=>{
   localStorage.setItem(
-    "Tasky",
+    "task",
     JSON.stringify({
       tasks: state.tasklist,
     })
@@ -104,7 +104,7 @@ const updateLocalStorage=()=>{
 
 // load initial data
  const loadInitialData=()=>{
-   const localStorageCopy=JSON.parse(localStorage.tasks);
+   const localStorageCopy=JSON.parse(localStorage.task);
    if(localStorageCopy) state.tasklist=localStorageCopy.tasks;
    state.tasklist.map((cardDate)=>{
     taskContents.insertAdjacentHTML("beforeend",htmlTaskContent(cardDate));
@@ -116,12 +116,15 @@ const updateLocalStorage=()=>{
    const input={
     url: document.getElementById("imgURL").value,
     title: document.getElementById("taskTitle").value,
-    tags: document.getElementById("tags").value,
-    taskDescription: document.getElementById("taskDesc").value,
+    type: document.getElementById("tags").value,
+    description: document.getElementById("taskDesc").value,
    };
-   taskContents.insertAdjacentHTML("beforeend",htmlTaskContent({...input,id,}));
+   if(input.title=="" || input.tags=="" || input.taskDescription==""){
+    return alert("please fill all necessary fields");
+   };
+   taskContents.insertAdjacentHTML("beforeend",htmlTaskContent({...input,id}));
    
-   state.tasklist.push({...input,id,});
+   state.tasklist.push({...input,id});
    updateLocalStorage();
 
  };
